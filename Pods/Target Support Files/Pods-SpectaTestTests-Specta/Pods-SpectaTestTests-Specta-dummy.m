154070ad3fcf17378d6791014af03d9452cb04cd
Marek Mościchowski <marek.moscichowski@miquido.com>
2015-05-01 11:13:32 +0200

    removed pods

--- Pods/Target Support Files/Pods-SpectaTestTests-Specta/Pods-SpectaTestTests-Specta-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SpectaTestTests_Specta : NSObject
-@end
-@implementation PodsDummy_Pods_SpectaTestTests_Specta
-@end
--- Pods/Target Support Files/Pods-SpectaTestTests/Pods-SpectaTestTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SpectaTestTests : NSObject
-@end
-@implementation PodsDummy_Pods_SpectaTestTests
-@end
